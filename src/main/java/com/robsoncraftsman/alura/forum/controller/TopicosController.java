fae300483cf3b03d0765cb9cb013f4d726721e3d
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2020-11-11 17:50:27 -0300

    Ajustado para permitir o filtro de tópicos pelo nome do curso

--- src/main/java/com/robsoncraftsman/alura/forum/controller/TopicosController.java
+++ src/main/java/com/robsoncraftsman/alura/forum/controller/TopicosController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.robsoncraftsman.alura.forum.controller.dto.TopicoDto;
+import com.robsoncraftsman.alura.forum.modelo.Topico;
 import com.robsoncraftsman.alura.forum.repository.TopicoRepository;
 
 @RestController
@@ -16,8 +17,13 @@ public class TopicosController {
 	private TopicoRepository topicoRepository;
 
 	@RequestMapping("/topicos")
-	public List<TopicoDto> listar() {
-		final var topicos = this.topicoRepository.findAll();
+	public List<TopicoDto> listar(final String nomeCurso) {
+		List<Topico> topicos;
+		if (nomeCurso == null) {
+			topicos = this.topicoRepository.findAll();
+		} else {
+			topicos = this.topicoRepository.findByCursoNome(nomeCurso);
+		}
 
 		return TopicoDto.convert(topicos);
 	}
--- src/main/java/com/robsoncraftsman/alura/forum/repository/TopicoRepository.java
+++ src/main/java/com/robsoncraftsman/alura/forum/repository/TopicoRepository.java
@@ -1,9 +1,13 @@
 package com.robsoncraftsman.alura.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.robsoncraftsman.alura.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	List<Topico> findByCursoNome(String nomeCurso);
 
 }
