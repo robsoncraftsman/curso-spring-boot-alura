45910ed3b275cf7bda6efb5966c75859052e49e0
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2020-11-13 09:49:19 -0300

    Adicionado endpoint para excluir os dados de um tópico

--- src/main/java/com/robsoncraftsman/alura/forum/controller/TopicosController.java
+++ src/main/java/com/robsoncraftsman/alura/forum/controller/TopicosController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,6 +69,13 @@ public class TopicosController {
 		final var topico = atualizarTopicoForm.atualizar(id, this.topicoRepository);
 
 		return ResponseEntity.ok(new TopicoDto(topico));
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> remover(@PathVariable final Long id) {
+		this.topicoRepository.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 
 }
